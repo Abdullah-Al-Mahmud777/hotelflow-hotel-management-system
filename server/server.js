05b7c0f053569d76b45eb6872330cc8a80c60cc7
Abdullah-Al-Mahmud <abdullahalmahmudmahmud777@gmail.com>
2026-01-03 02:10:42 +0600

    Initial commit - setup server and client

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose"); // important
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.send("HotelFlow API is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
